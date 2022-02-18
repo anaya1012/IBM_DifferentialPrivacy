10d132dd94e7acc70a43fd032f88e7dc61da882e
Aditijo2000 <56394990+Aditijo2000@users.noreply.github.com>
2022-02-18 09:57:21 +0530

    tabs

---
+++ WithDP.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WithDP = () =>{
+    return <div>This is with dp tab</div>;
+}
+
+export default WithDP;
---
+++ WithoutDP.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WithoutDP = () =>{
+    return <div>This is without dp tab</div>;
+}
+
+export default WithoutDP;

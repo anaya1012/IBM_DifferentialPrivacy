edd7cb1a24fdc4aea32db2489bdf59acd97edd66
Aditijo2000 <56394990+Aditijo2000@users.noreply.github.com>
2022-03-09 22:19:55 +0530

    Add files via upload

---
+++ components/controls/Controls.js
@@ -0,0 +1,16 @@
+
+import RadioGroup from "./RadioGroup";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./Select";
+import Checkbox from "./Checkbox";
+const Controls = {
+    RadioGroup,
+    Button,
+    Input,
+    Checkbox,
+    Select
+
+}
+
+export default Controls;

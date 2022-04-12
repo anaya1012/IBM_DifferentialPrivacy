338146984a090c17f5c6ce175ab074207d62c3af
Aditijo2000 <56394990+Aditijo2000@users.noreply.github.com>
2022-04-12 15:52:17 +0530

    Add files via upload

---
+++ pages/DataContributerMain.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { CssBaseline, Paper, makeStyles } from '@material-ui/core'
+import { padding } from '@mui/system'
+import FileUpload from '../components/FileUpload';
+
+const useStyle = makeStyles(theme => ({
+    pageContent :{
+        
+        margin: theme.spacing(5),
+        padding: theme.spacing(3)
+
+    }
+}))
+
+
+export default function DataContributerMain() {
+
+    const classes = useStyle();
+  return (
+    
+    <>
+    
+    
+
+    <Paper
+    style={{ marginLeft: "300px", border: "1px solid black" }}
+    className={classes.pageContent}>
+      <div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+      <i className='fab fa-react' /> Dataset Upload
+    </h4>  </div>
+      <FileUpload />
+    </Paper>
+    
+
+    
+    
+    
+   
+    </>
+        
+    
+    
+  )
+}
